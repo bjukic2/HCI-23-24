8d9fdecaed0657b3e8cca94c7a229ccc7be3f0c3
bjukic2 <brunojukic68@gmail.com>
2024-01-18 19:12:53 +0100

    Added custom not found page

---
+++ semester-project/app/not-found.tsx
@@ -0,0 +1,14 @@
+import Link from "next/link";
+import "./globals.css";
+import closed_icon from "@/components/icons/4848452_closed_commerce_shop_sign_signaling_icon.png";
+import Image from "next/image";
+
+export default function NotFound(){
+    return(
+        <main className="not-found">
+            <Image className="icon" src={closed_icon} alt=""/>
+            <p>Žao nam je, nismo mogli pronaći stranicu koju tražite.</p>
+            <p>Vratite se na <Link className="pocetna" href="/">Početnu</Link>.</p>
+        </main>
+    )
+}
