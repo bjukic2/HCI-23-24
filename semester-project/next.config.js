459bafdc10e83efb1362ff53e128735cbc9850cb
bjukic2 <brunojukic68@gmail.com>
2024-06-22 19:29:01 +0200

    Added safe way of calling space id and bearer token

--- semester-project/next.config.js
+++ semester-project/next.config.js
@@ -3,6 +3,11 @@ const nextConfig = {
   images: {
     domains: ['images.ctfassets.net', 'tommy.hr'],
   },
+  env: {
+    NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN: process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN,
+    NEXT_PUBLIC_CONTENTFUL_SPACE_ID: process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID,
+  },
 };
 
 module.exports = nextConfig;
+
